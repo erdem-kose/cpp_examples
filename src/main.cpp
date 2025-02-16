51ed6dcbde06625fc4c539f7adbb6e6ef6727756
Erdem Köse <erdemkose78@gmail.com>
2025-02-16 20:11:44 +0000

    googletest with ctest example added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
+#include <stdlib.h>
 #include "raylib/example.hpp"
+#include "gtest/gtest.h"
 
-int main(void)
-{
+int add(int a, int b) {
+    return a + b;
+}
+
+TEST(MyMathTest, AddTest) {
+    EXPECT_EQ(add(1, 2), 3);
+    EXPECT_EQ(add(-1, -2), -3);
+    EXPECT_EQ(add(0, 0), 0);
+}
+
+int main(int argc, char **argv) {
+    system("CLS");
+
+    ::testing::InitGoogleTest(&argc, argv);
+
     //raylib_example();
+
+    return RUN_ALL_TESTS();
 }
